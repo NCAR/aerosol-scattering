e29af438fdbd4569e2a27d865fb50cd762ae9bfa
Matthew Hayman <mhayman@ucar.edu>
2026-04-03 11:02:39 -0600

    update notes on echo optimization in bash script

--- scripts/casper/bash/run_echo.bash
+++ scripts/casper/bash/run_echo.bash
@@ -10,6 +10,9 @@ echo-opt scripts/casper/echo_config/cached_hyper.yml scripts/casper/echo_config/
 
 # get report and best model
 # echo-report scripts/casper/echo_config/cached_hyper.yml -m scripts/casper/echo_config/polynn_model_config.yml 
+# get report and best model in the run output directory
+# module load conda
+# conda activate holodec
 # echo-report cached_hyper.yml -m polynn_model_config.yml 
 
-# cp best.yml ~/Python/aerosol-scattering/config/echo/best_beta3_alpha2_r_eff_all_index.yml
+# cp best.yml ~/Python/aerosol-scattering/config/echo/best_beta1_alpha1_532_r_eff_all_index.yml
